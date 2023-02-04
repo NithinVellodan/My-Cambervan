6a5eff1fc242a6f6c499fe64694effe3a7e4327d
NithinVellodan <nithinbabuvellodan@gmail.com>
2023-02-04 15:59:16 +0530

    functions

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 function Popup() {
-  return <Div>
-    
-  </Div>;
+  const [images, setImages] = useState([
+    {
+      id: 1,
+      image: require("../assets/caravan/image1.jpg"),
+    },
+    {
+      id: 2,
+      image: require("../assets/caravan/image2.jpg"),
+    },
+    {
+      id: 3,
+      image: require("../assets/caravan/image3.jpg"),
+    },
+    {
+      id: 4,
+      image: require("../assets/caravan/image4.jpg"),
+    },
+    {
+      id: 5,
+      image: require("../assets/caravan/image5.jpg"),
+    },
+    {
+      id: 6,
+      image: require("../assets/caravan/image6.jpg"),
+    },
+    {
+      id: 7,
+      image: require("../assets/caravan/image7.jpg"),
+    },
+    {
+      id: 8,
+      image: require("../assets/caravan/image8.jpg"),
+    },
+    {
+      id: 9,
+      image: require("../assets/caravan/image9.jpg"),
+    },
+    {
+      id: 10,
+      image: require("../assets/caravan/image10.jpg"),
+    },
+  ]);
+
+  const renderImg = () => {
+    return images.map((image) => {
+      <Items>
+        <Img src={image.image} />
+      </Items>;
+    });
+  };
+
+  return (
+    <ImageDiv>
+      <List>{renderImg}</List>
+    </ImageDiv>
+  );
 }
 
 export default Popup;
@@ -26,3 +79,10 @@ const Div = styled.div`
     height: 230px;
   }
 `;
+const Items = styled.div`
+display: block;
+width: 100%;
+`;
+const Img = styled.img``;
+const ImageDiv = styled.div``;
+const List = styled.div``;
